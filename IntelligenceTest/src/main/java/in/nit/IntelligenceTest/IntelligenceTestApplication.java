6aa1799c58beb7006700a5b1e95c69e4bc8b5f19
sankar <karrasankar09@gmail.com>
2020-03-22 11:51:08 +0530

    using spring initializer to create spring project and unzip import

---
+++ IntelligenceTest/src/main/java/in/nit/IntelligenceTest/IntelligenceTestApplication.java
@@ -0,0 +1,14 @@
+package in.nit.IntelligenceTest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntelligenceTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntelligenceTestApplication.class, args);
+		System.out.println("created by using spring initializer");
+	}
+
+}
